bad3bd8ee2a798670990837f8b2a4e3e7fcfca34
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-02-15 13:19:35 -0500

    Revert "trial commit"
    This reverts commit 0ea2a994905358d5a9d698d5abb213f7c6643503.

--- src/main/java/com/mycompany/frogsandtoads/PrimaryController.java
+++ src/main/java/com/mycompany/frogsandtoads/PrimaryController.java
@@ -2,12 +2,8 @@ package com.mycompany.frogsandtoads;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class PrimaryController {
-
-    @FXML
-    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
