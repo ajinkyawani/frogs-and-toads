257172514d1d2910f2c76258b59161321c19684a
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-02-15 13:22:55 -0500

    minor fixes

--- src/main/java/com/mycompany/frogsandtoads/SecondaryController.java
+++ src/main/java/com/mycompany/frogsandtoads/SecondaryController.java
@@ -2,8 +2,12 @@ package com.mycompany.frogsandtoads;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
+
+    @FXML
+    private Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
